bc226249f70f168f2f289d1045336a9b96c48f89
b2212100 <hiraku0209@icloud.com>
2022-11-01 19:32:47 +0900

    課題1-1作成

---
+++ prmn2022a/prmn2022a/src/lecture01/kadai1_1.java
@@ -0,0 +1,12 @@
+package lecture01;
+
+public class kadai1_1 {
+    public static void main(String[] args) {
+        // 自分の学籍番号、名前を表示
+        String number = "B2212100";
+        String name = "Hiraku Hosogoshi";
+
+        System.out.println("学籍番号 : 名前");
+        System.out.println(number + " : " + name);
+    }
+}
