5b2a87979533b3f46925108083e915f516027460
b2212100 <hiraku0209@icloud.com>
2022-11-05 01:36:37 +0900

    課題2-1(Humanクラス)作成

---
+++ prmn2022a/prmn2022a/src/lecture02/Exercise2_1.java
@@ -0,0 +1,14 @@
+package lecture02;
+
+public class Exercise2_1 {
+    public static void main(String[] args) {
+        Human student1 = new Human("Araki Ikara", 18);
+        student1.print();
+
+        Human student2 = new Human("Ukare Eraku", 22);
+        student2.print();
+
+        Human worker = new Human("Ato Ninen", 23);
+        worker.print();
+    }
+}
---
+++ prmn2022a/prmn2022a/src/lecture02/Human.java
@@ -0,0 +1,22 @@
+package lecture02;
+
+public class Human {
+    String name;
+    int age;
+
+    Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void print() {
+        if (age <= 18) {
+            System.out.println(this.name + " (生徒) " + this.age + "歳");
+        } else if (age <= 22) {
+            System.out.println(this.name + " (学生) " + this.age + "歳");
+        } else {
+            System.out.println(this.name + " " + this.age + "歳");
+        }
+        return;
+    }
+}
