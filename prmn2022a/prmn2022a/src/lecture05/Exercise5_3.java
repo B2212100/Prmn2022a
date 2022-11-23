2398cde5c4182aac281c3db67719a7e4ce1ffba3
b2212100 <hiraku0209@icloud.com>
2022-11-23 10:47:42 +0900

    課題5-1~7作成

--- prmn2022a/prmn2022a/src/lecture05/Exercise5_3.java
+++ prmn2022a/prmn2022a/src/lecture05/Exercise5_3.java
@@ -3,7 +3,7 @@ package lecture05;
 public class Exercise5_3 {
     public static void main(String[] args) {
         // Butterfly クラスのインスタンスを Insect 型の変数に代入
-        Insect I_But = new Butterfly();
-        I_But.move();
+        Insect insBut = new Butterfly();
+        insBut.move();
     }
 }
