171499850496133109a500ddcfd9ce2c9a742fc1
b2212100 <hiraku0209@icloud.com>
2022-12-02 23:58:21 +0900

    課題6-1作成

---
+++ prmn2022a/prmn2022a/src/lecture06/Exercise6_1.java
@@ -0,0 +1,19 @@
+package lecture06;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+// 想定される値が入力されなかった場合の例外処理
+public class Exercise6_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("小数値を入力してください:\n> ");
+
+        try {
+            double input = scanner.nextDouble();
+            System.out.println("input = " + input);
+        } catch (InputMismatchException e) {
+            System.out.println("エラー.");
+        }
+    }
+}
